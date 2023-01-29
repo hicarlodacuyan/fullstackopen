23549b9dc230dccde7baa6fe8a4f538088ccefd3
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2023-01-29 21:23:28 +0800

    Squashed 'part9/patientor_backend/' content from commit cae5b4d
    git-subtree-dir: part9/patientor_backend
    git-subtree-split: cae5b4de3738e90150cb3b0118316db45ba9a678

---
+++ src/routes/patients.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import patientService from '../services/patientService';
+import toNewPatientEntry from '../utils/toNewPatientEntry';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  res.send(patientService.getNonSensitivePatient());
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newPatientEntry = toNewPatientEntry(req.body);
+
+    const addedEntry = patientService.addPatient(newPatientEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+
+    if (error instanceof Error) {
+      errorMessage += 'Error: ' + errorMessage;
+    }
+
+    res.status(400).send(errorMessage);
+  }
+});
+
+export default router;
